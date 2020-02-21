b2ae9b426297485f510c5e4808acf39eb986bae5
Zilu Ramkrishna Rane <zilurrane@gmail.com>
2020-02-21 22:46:49 +0530

    Updated Node version & rimraf to fix PCF deployment issue

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import app from "./app";
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-    console.log('Express server listening on port ' + PORT);
+    console.log('FbApp API server listening on port ' + PORT);
 })

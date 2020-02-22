53e5cf839b65b7a69b3556eceeaadcfdc37de9de
Zilu Ramkrishna Rane <zilurrane@gmail.com>
2020-02-22 21:35:38 +0530

    GetAll and Single subject - GraphQL

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,14 +1,30 @@
 import { ApolloServer, gql } from 'apollo-server-express';
+import { SubjectModel } from '../models/subject.model';
+import DateScalar from './scalars/date';
 
 const typeDefs = gql`
+    scalar Date
+    type Subject {
+        id: ID!
+        code: String
+        name: String
+        departmentCode: String
+        classCode: String
+        parameters: [String]
+        isActive: Boolean
+        createdDate: Date
+    }
     type Query {
-        hello: String
+        subjects: [Subject]
+        subject(id: ID!): Subject
     }
 `;
 
 const resolvers = {
+    Date: DateScalar,
     Query: {
-        hello: () => 'Hello World!'
+        subjects: async () => await SubjectModel.find({}).exec(),
+        subject: async (_parent: any, { id }: any) => await SubjectModel.findById(id).exec()
     }
 };
 

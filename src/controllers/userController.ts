24edc96b8d121c9290f0ea5707a4efe188b86871
Zilu Ramkrishna Rane <zilurrane@gmail.com>
2020-02-29 17:22:10 +0530

    Dummy login API

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express'
+import { StudentModel } from '../models/Student.model';
 
 export class UserController {
 
@@ -6,8 +7,17 @@ export class UserController {
         res.status(200).json([])
     }
 
+    // TODO: Refactor this
     public login(req: Request, res: Response) {
-        res.status(200).json({})
+
+        const { userName, password } = req.body;
+        StudentModel.findOne({ userName, password })
+            .then((response) => {
+                res.send(response);
+            })
+            .catch((err) => {
+                res.status(500).send(err);
+            });
     }
 
     public createNewUser(req: Request, res: Response) {

d1265ac3a4a65df901bf46638f621f48184af554
Zilu Ramkrishna Rane <zilurrane@gmail.com>
2020-04-25 19:46:29 +0530

    Sort feedback parameters by code

--- src/graphql/resolvers/feedback-parameter.resolver.ts
+++ src/graphql/resolvers/feedback-parameter.resolver.ts
@@ -2,10 +2,17 @@ import { getTenantBoundFeedbackParameterModel } from '../../models/feedback-para
 
 export default {
     Query: {
-        feedbackParameters: async (_parent: any, _args: any, context : any) => await getTenantBoundFeedbackParameterModel(context).find({}).exec(),
+        feedbackParameters: async (_parent: any, _args: any, context: any) => {
+            const feedbackParameters = await getTenantBoundFeedbackParameterModel(context).find({}).exec();
+            return feedbackParameters.sort(function (first: any, second: any) {
+                if (first.code < second.code) { return -1; }
+                if (second.code > first.code) { return 1; }
+                return 0;
+            });
+        }
     },
     Mutation: {
-        addFeedbackParameter: async (_parent: any, args: any, context : any) => {
+        addFeedbackParameter: async (_parent: any, args: any, context: any) => {
             try {
                 const feedbackParameterToInsert = new (getTenantBoundFeedbackParameterModel(context))(args);
                 let response = await feedbackParameterToInsert.save();
